fadddaadc7dd8e7471abd2f37a01398af7e26495
marian.sabianaa <marian.sabianaa@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-09 06:38:38 +0000

    add archivos para constraints de business

--- trunk/queries/business_constraints.sql
+++ trunk/queries/business_constraints.sql
@@ -1,3 +1,14 @@
 -- ACA VAN TODAS LAS CONSTRAINTS DE LOGICA DE NEGOCIO QUE PIDEN QUE NO SEAN PKS Y FKS -- 
 
+-- Se abren desde archivos separados para que sea mas prolijo -- 
+
+source constraints/arbitroConstraints.sql
+source constraints/partidoConstraints.sql
+source constraints/jugadorConstraints.sql
 source constraints/seleccionConstraints.sql
+source constraints/etapaConstraints.sql
+source constraints/posicionConstraints.sql
+source constraints/participacionConstraints.sql
+source constraints/tanteadorConstraints.sql
+source constraints/integranteConstraints.sql
+source constraints/sancionConstraints.sql
---
+++ trunk/queries/constraints/participacionConstraints.sql
@@ -0,0 +1,3 @@
+-- ACA VAN TODAS LAS CONSTRAINTS DE SELECCION
+
+select "Acá van las constraints de participacion" from DUAL;
