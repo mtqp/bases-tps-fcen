181593069aeabde2bcd7a67ce531260388f91b75
marian.sabianaa <marian.sabianaa@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-10 20:35:55 +0000

    jajajajajajaja

--- trunk/queries/constraints/posicionConstraints.sql
+++ trunk/queries/constraints/posicionConstraints.sql
@@ -3,9 +3,9 @@
 
 DELIMITER $$
 
-DROP TRIGGER IF EXISTS check_posicion $$
+DROP TRIGGER IF EXISTS check_posicion_bi $$
 
-CREATE TRIGGER check_posicion
+CREATE TRIGGER check_posicion_bi
 BEFORE INSERT ON posicion
 FOR EACH ROW BEGIN
     -- POSICION.puntos >= 0
@@ -37,6 +37,42 @@ FOR EACH ROW BEGIN
 	CALL `PartidosJugados = partidosGanados + partidosPerdidos`;
     END IF;
 END$$
+
+DROP TRIGGER IF EXISTS check_posicion_bu $$
+
+CREATE TRIGGER check_posicion_bi
+BEFORE UPDATE ON posicion
+FOR EACH ROW BEGIN
+    -- POSICION.puntos >= 0
+    IF (NEW.puntos < 0) THEN
+	CALL `Puntos debe ser >= 0`;
+    END IF;
+    -- POSICION.partidosJugados >= 0
+    IF (NEW.partidosJugados < 0) THEN
+	CALL `PartidosJugados debe ser >= 0`;
+    END IF;
+    -- POSICION.partidosGanados >= 0
+    IF (NEW.partidosGanados < 0) THEN
+	CALL `PartidosGanados debe ser >= 0`;
+    END IF;
+    -- POSICION.partidosPerdidos >= 0
+    IF (NEW.partidosPerdidos < 0) THEN
+	CALL `PartidosPerdidos debe ser >= 0`;
+    END IF;
+    -- POSICION.tantosAFavor >= 0
+    IF (NEW.tantosAFavor < 0) THEN
+	CALL `TantosAFavor debe ser >= 0`;
+    END IF;
+    -- POSICION.tantosEnContra >= 0
+    IF (NEW.tantosEnContra < 0) THEN
+	CALL `TantosEnContra debe ser >= 0`;
+    END IF;
+    -- partidosJugados = partidosGanados + partidosPerdidos
+    IF (NEW.partidosJugados <> NEW.partidosGanados + NEW.partidosPerdidos) THEN
+	CALL `PartidosJugados = partidosGanados + partidosPerdidos`;
+    END IF;
+END$$
+
 DELIMITER ;
 
 -- Si POSICION.puntos es el mayor de todos => POSICION.partidosGanados >=2
