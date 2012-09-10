443c373c7bcecfb59959c5dc07d4624f84443e93
martincammi@gmail.com <martincammi@gmail.com@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-10 02:10:09 +0000

    Agregado de CheckConstraints

--- trunk/queries/constraints/seleccionConstraints.sql
+++ trunk/queries/constraints/seleccionConstraints.sql
@@ -1,3 +1,21 @@
 -- ACA VAN TODAS LAS CONSTRAINTS DE SELECCION
+-- select "Acá van las constraints de seleccion" from DUAL;
 
-select "Acá van las constraints de seleccion" from DUAL;
+-- seleccion --
+-- SELECCION.grupo solo puede ser ‘A’ o ‘B’.
+-- ALTER TABLE seleccion ADD CONSTRAINT chk_grupo CHECK (grupo='A' OR grupo='B'); -- Constraint puto no anda. 
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS check_grupo $$
+
+CREATE TRIGGER check_grupo
+BEFORE INSERT ON seleccion
+FOR EACH ROW BEGIN
+  IF (NEW.grupo <> 'A' AND NEW.grupo <> 'B') THEN
+	  CALL `Grupo debe ser A o B`;
+  END IF;
+END$$
+DELIMITER ;
+
+
---
+++ trunk/queries/truncateDb.sql
@@ -0,0 +1,21 @@
+
+-- Borra todos los datos de la base de datos.
+
+TRUNCATE seleccion;
+TRUNCATE posicion;
+TRUNCATE lugarhospedaje;
+TRUNCATE pais;
+TRUNCATE integrante;
+TRUNCATE jugador;
+TRUNCATE cuerpotecnico;
+TRUNCATE funcion;
+TRUNCATE equipo;
+TRUNCATE partido;
+TRUNCATE estadio;
+TRUNCATE etapa;
+TRUNCATE tanteador;
+TRUNCATE arbitro;
+TRUNCATE arbitra;
+TRUNCATE participacion;
+TRUNCATE sancion;
+TRUNCATE tiposancion;
