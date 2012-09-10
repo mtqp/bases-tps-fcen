a299a3d60e23286b77ed20c4ba5e751e4f31c0b4
noeliaserapio@gmail.com <noeliaserapio@gmail.com@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-10 15:45:19 +0000

    algunas constraints (faltan igual algunas de los archivos subidos)

--- queries/constraints/posicionConstraints.sql
+++ queries/constraints/posicionConstraints.sql
@@ -1,3 +1,45 @@
 -- ACA VAN TODAS LAS CONSTRAINTS DE SELECCION
+-- select "Acá van las constraints de posicion" from DUAL;
 
-select "Acá van las constraints de posicion" from DUAL;
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS check_posicion $$
+
+CREATE TRIGGER check_posicion
+BEFORE INSERT ON posicion
+FOR EACH ROW BEGIN
+    -- POSICION.puntos >= 0
+    IF (NEW.puntos < 0) THEN
+	CALL `Puntos debe ser >= 0`;
+    END IF;
+    -- POSICION.partidosJugados >= 0
+    IF (NEW.partidosJugados < 0) THEN
+	CALL `PartidosJugados debe ser >= 0`;
+    END IF;
+    -- POSICION.partidosGanados >= 0
+    IF (NEW.partidosGanados < 0) THEN
+	CALL `PartidosGanados debe ser >= 0`;
+    END IF;
+    -- POSICION.partidosPerdidos >= 0
+    IF (NEW.partidosPerdidos < 0) THEN
+	CALL `PartidosPerdidos debe ser >= 0`;
+    END IF;
+    -- POSICION.tantosAFavor >= 0
+    IF (NEW.tantosAFavor < 0) THEN
+	CALL `TantosAFavor debe ser >= 0`;
+    END IF;
+    -- POSICION.tantosEnContra >= 0
+    IF (NEW.tantosEnContra < 0) THEN
+	CALL `TantosEnContra debe ser >= 0`;
+    END IF;
+    -- partidosJugados = partidosGanados + partidosPerdidos
+    IF (NEW.partidosJugados <> NEW.partidosGanados + NEW.partidosPerdidos) THEN
+	CALL `PartidosJugados = partidosGanados + partidosPerdidos`;
+    END IF;
+END$$
+DELIMITER;
+
+-- Si POSICION.puntos es el mayor de todos => POSICION.partidosGanados >=2
+-- (minimo debe ganar dos partidos en fase de grupos para ser campéon)
+
+
