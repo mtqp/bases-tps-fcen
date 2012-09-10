1afc6d942d3d5a09e3c27c020e789b3df2679e9d
noeliaserapio@gmail.com <noeliaserapio@gmail.com@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-10 15:45:19 +0000

    algunas constraints (faltan igual algunas de los archivos subidos)

--- trunk/queries/constraints/integranteConstraints.sql
+++ trunk/queries/constraints/integranteConstraints.sql
@@ -1,3 +1,20 @@
 -- ACA VAN TODAS LAS CONSTRAINTS DE SELECCION
+-- select "Acá van las constraints de integrante" from DUAL;
 
-select "Acá van las constraints de integrante" from DUAL;
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS check_integrante $$
+
+CREATE TRIGGER check_integrante
+BEFORE INSERT ON integrante
+FOR EACH ROW BEGIN
+    -- INTEGRANTE.tipoIntegrante IN { ‘Jugador’, ‘CuerpoTecnico’}
+    IF (NEW.tipoIntegrante <> 'Jugador' AND NEW.tipoIntegrante <> 'CuerpoTecnico') THEN
+        CALL `TipoIntegrante debe ser Jugador o CuerpoTecnico`;
+    END IF;
+    -- AÑO(SYSDATE) - AÑO(INTEGRANTE.fechaNacimiento) >= 18
+    IF (YEAR(SYSDATE()) - YEAR(NEW.fechaNacimiento) < 18) THEN  
+	CALL `AÑO(SYSDATE) - AÑO(INTEGRANTE.fechaNacimiento) debe ser >= 18`;
+    END IF;
+END$$
+DELIMITER ;
--- trunk/queries/constraints/partidoConstraints.sql
+++ trunk/queries/constraints/partidoConstraints.sql
@@ -1,3 +1,22 @@
 -- ACA VAN TODAS LAS CONSTRAINTS DE SELECCION
+--select "Acá van las constraints de partido" from DUAL;
 
-select "Acá van las constraints de partido" from DUAL;
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS check_partido $$
+
+CREATE TRIGGER check_partido
+BEFORE INSERT ON partido
+FOR EACH ROW BEGIN
+    -- PARTIDO.equipoSeleccion1 <> PARTIDO.equipoSeleccion2
+    IF (NEW.equipoSeleccion1 = NEW.equipoSeleccion2) THEN
+	CALL `EquipoSeleccion1 debe ser distinto de equipoSeleccion2`;
+    END IF;
+    -- La duración de los partidos tiene que ser > 0
+    IF (NEW.duracion <= 0) THEN
+	CALL `La duración debe ser > 0`;
+    END IF;
+    -- Si PARTIDO.juegaEnEtapa = ‘FASE_GRUPOS’’entonces PARTIDO.equipoSeleccion1.grupo = PARTIDO.equipoSeleccion2.grupo
+END$$
+DELIMITER ;
+
