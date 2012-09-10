936171932862ef383f29f1191c8fcf0d4fffbebe
martin.cammi <martin.cammi@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-10 04:34:06 +0000

    Más datos de prueba

--- trunk/queries/data.sql
+++ trunk/queries/data.sql
@@ -1,7 +1,6 @@
 -- Acá iran los DML --
 
 -- PAIS
-
 INSERT INTO pais (nombrePais) values ('Argentina');
 INSERT INTO pais (nombrePais) values ('Bolivia');
 INSERT INTO pais (nombrePais) values ('Brasil');
@@ -13,27 +12,36 @@ INSERT INTO pais (nombrePais) values ('Uruguay');
 INSERT INTO pais (nombrePais) values ('Venezuela');
 
 -- HOSPEDAJE --
-
 INSERT INTO lugarhospedaje (nombreHospedaje) values ('Hotel Intercontinental');
 INSERT INTO lugarhospedaje (nombreHospedaje) values ('Hotel Savoy');
 INSERT INTO lugarhospedaje (nombreHospedaje) values ('Hotel Hillton');
 INSERT INTO lugarhospedaje (nombreHospedaje) values ('Hotel Sheraton');
 
 -- FUNCION --
-
 INSERT INTO funcion (nombreFuncion) values ('CLINICO');
 INSERT INTO funcion (nombreFuncion) values ('TRAUMATOLOGO');
 INSERT INTO funcion (nombreFuncion) values ('PSICOLOGO');
 
 -- ESTADIO --
-
 INSERT INTO estadio (nombreEstadio) values ('ElGranBalon');
 INSERT INTO estadio (nombreEstadio) values ('MikeJordan');
 INSERT INTO estadio (nombreEstadio) values ('BolaAlCesto');
 
--- SELECCION --
+-- ETAPA --
+INSERT INTO etapa (nombreEtapa) values ('FASE_GRUPOS');
+INSERT INTO etapa (nombreEtapa) values ('5TO_PUESTO');
+INSERT INTO etapa (nombreEtapa) values ('SEMIFINAL');
+INSERT INTO etapa (nombreEtapa) values ('3ER_PUESTO');
+INSERT INTO etapa (nombreEtapa) values ('FINAL');
 
--- Validas --
+-- TIPO_SANCION --
+INSERT INTO tiposancion (nombreSancion) values ('BLOQUEO');
+INSERT INTO tiposancion (nombreSancion) values ('EMPUJE');
+INSERT INTO tiposancion (nombreSancion) values ('TRABA');
+INSERT INTO tiposancion (nombreSancion) values ('GOLPE');
+INSERT INTO tiposancion (nombreSancion) values ('PUÑETAZO');
+
+-- SELECCION --
 INSERT INTO seleccion (cantIntegrantes, fechaArribo, grupo, hospedaHospedaje, representaPais, concentraEstadio) values (0,'2012-08-20','A', 1, 1, 1);
 INSERT INTO seleccion (cantIntegrantes, fechaArribo, grupo, hospedaHospedaje, representaPais, concentraEstadio) values (0,'2012-08-21','B', 2, 2, 2);
 INSERT INTO seleccion (cantIntegrantes, fechaArribo, grupo, hospedaHospedaje, representaPais, concentraEstadio) values (0,'2012-08-22','A', 3, 3, 3);
@@ -41,7 +49,6 @@ INSERT INTO seleccion (cantIntegrantes, fechaArribo, grupo, hospedaHospedaje, re
 INSERT INTO seleccion (cantIntegrantes, fechaArribo, grupo, hospedaHospedaje, representaPais, concentraEstadio) values (0,'2012-08-24','A', 1, 5, 2);
 
 -- Invalidas --
-
 -- La cantidad de Integrantes no coincide con los existentes  
 -- INSERT INTO seleccion (cantIntegrantes,fechaArribo,grupo) values (20,'2012-08-25','B');
 
