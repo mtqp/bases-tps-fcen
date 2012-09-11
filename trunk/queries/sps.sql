88cff1fec8d748b047b65f12714ce7ac175859be
marian.sabianaa <marian.sabianaa@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-11 22:04:12 +0000

    log error automatico

--- trunk/queries/sps.sql
+++ trunk/queries/sps.sql
@@ -1,8 +1,8 @@
 -- Generado automaticamente
 
-source ./sps/martingaparece.sql;
 source ./sps/sp_log_error.sql;
 source ./sps/business_sps.sql;
 source ./sps/generics/sp_valor_en_rango.sql;
 source ./sps/generics/sp_valor_positivo.sql;
+source ./sps/partido/sp_partido_mismos_equipos_misma_etapa.sql;
 source ./sps/partido/sp_partido_distintas_selecciones.sql;
