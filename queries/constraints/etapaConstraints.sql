ac8727e10a4555c1cdd53abe0f981a579b406bcf
noeliaserapio@gmail.com <noeliaserapio@gmail.com@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-11 00:09:10 +0000

    AND en mayusculas

--- queries/constraints/etapaConstraints.sql
+++ queries/constraints/etapaConstraints.sql
@@ -9,7 +9,7 @@ CREATE TRIGGER check_etapa_bi
 BEFORE INSERT ON etapa
 FOR EACH ROW BEGIN
     -- ETAPA.nombreEtapa debe ser o bien FASE_GRUPOS o 5TO_PUESTO, o 3ER_PUESTO o SEMIFINAL, o FINAL
-    IF (NEW.nombreEtapa <> 'FASE_GRUPOS' and NEW.nombreEtapa <> '5TO_PUESTO' and NEW.nombreEtapa <> '3ER_PUESTO' and NEW.nombreEtapa <> 'SEMIFINAL' and NEW.nombreEtapa <> 'FINAL') THEN
+    IF (NEW.nombreEtapa <> 'FASE_GRUPOS' AND NEW.nombreEtapa <> '5TO_PUESTO' AND NEW.nombreEtapa <> '3ER_PUESTO' AND NEW.nombreEtapa <> 'SEMIFINAL' AND NEW.nombreEtapa <> 'FINAL') THEN
 	CALL `NombreEtapa es FASE_GRUPOS, 5TO/3ER_PUESTO, SEMIFINAL o FINAL`;
     END IF;
 END$$
@@ -20,7 +20,7 @@ CREATE TRIGGER check_etapa_bu
 BEFORE UPDATE ON etapa
 FOR EACH ROW BEGIN
     -- ETAPA.nombreEtapa debe ser o bien FASE_GRUPOS o 5TO_PUESTO, o 3ER_PUESTO o SEMIFINAL, o FINAL
-    IF (NEW.nombreEtapa <> 'FASE_GRUPOS' and NEW.nombreEtapa <> '5TO_PUESTO' and NEW.nombreEtapa <> '3ER_PUESTO' and NEW.nombreEtapa <> 'SEMIFINAL' and NEW.nombreEtapa <> 'FINAL') THEN
+    IF (NEW.nombreEtapa <> 'FASE_GRUPOS' AND NEW.nombreEtapa <> '5TO_PUESTO' AND NEW.nombreEtapa <> '3ER_PUESTO' AND NEW.nombreEtapa <> 'SEMIFINAL' AND NEW.nombreEtapa <> 'FINAL') THEN
 	CALL `NombreEtapa es FASE_GRUPOS, 5TO/3ER_PUESTO, SEMIFINAL o FINAL`;
     END IF;
 END$$
