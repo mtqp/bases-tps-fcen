853f14ed1af247dce4d73f2e3dac371b0b58a973
marian.sabianaa <marian.sabianaa@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-11 21:52:50 +0000

    agregado de recreate SPSource

---
+++ trunk/queries/recreateSPSource.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+
+import os
+sqlfiles = []
+for root, dirs, files in os.walk('./sps'):
+  for file in files:
+    if file.endswith('.sql'):
+      sqlfiles.append(os.path.join(root, file))
+      
+sourceFile = open('./sps.sql', 'w')
+sourceFile.write('-- Generado automaticamente\n\n')
+
+for sqlFile in sqlfiles:
+    sourceFile.write('source ' + sqlFile + ';\n')
+
+sourceFile.close()
+
+#import os
+#os.chdir("./../sps")
+#for files in os.listdir("."):
+#    if files.endswith(".sql"):
+#        print files
