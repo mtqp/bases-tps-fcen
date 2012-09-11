19a127d0a2890a9d514f1655a36d00c4b4586c8e
marian.sabianaa <marian.sabianaa@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-11 23:42:10 +0000

    arreglando cajeteadas

--- queries/sps/generics/sp_valor_en_rango.sql
+++ queries/sps/generics/sp_valor_en_rango.sql
@@ -6,7 +6,6 @@ DROP PROCEDURE IF EXISTS sp_valor_en_rango &&
 CREATE PROCEDURE sp_valor_en_rango(valor INT, cotaInferior INT, cotaSuperior INT, caller VARCHAR(50), propiedad varchar(50))
 BEGIN
     IF (cotaInferior < valor OR cotaSuperior > valor ) THEN
-        CALL logError(caller, CONCAT(propiedad, '-', 'no esta en rango') );
 		CALL CONCAT('La propiedad ', propiedad, 'no esta en rango');
     END IF;
 END&&
