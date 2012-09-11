5f4d63f525dc2767130076f979a2b9a8a3675da3
marian.sabianaa <marian.sabianaa@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-11 23:42:10 +0000

    arreglando cajeteadas

--- trunk/queries/sps/generics/sp_valor_positivo.sql
+++ trunk/queries/sps/generics/sp_valor_positivo.sql
@@ -6,7 +6,6 @@ DROP PROCEDURE IF EXISTS sp_valor_positivo &&
 CREATE PROCEDURE sp_valor_positivo (valor INT, caller VARCHAR(50), propiedad varchar(50))
 BEGIN
     IF (valor <= 0) THEN
-        CALL logError(caller, CONCAT(propiedad, '-', 'debe ser mayor que 0') );
 		CALL CONCAT('La propiedad ', propiedad, 'debe ser mayor que 0');
     END IF;
 END&&
