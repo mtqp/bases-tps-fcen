f1c582c63284b0d03cc93d76884c6af429caa423
marian.sabianaa <marian.sabianaa@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-11 21:02:13 +0000

    creacion generics y utilizacion de sps en partido

--- queries/sps.sql
+++ queries/sps.sql
@@ -1,6 +1,8 @@
 -- Script de Stored Procedures --
 
 source sps/business_sps.sql;
-source sps/sp_log_error.sql
+source sps/sp_log_error.sql;
+source sps/generics/sp_valor_positivo.sql;
+source sps/partido/sp_partido_distintas_selecciones.sql;
 
 
