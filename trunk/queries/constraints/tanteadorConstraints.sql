b33b18b0ea0f7b5fff8568d22fba6b06f3655ba6
noeliaserapio@gmail.com <noeliaserapio@gmail.com@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-12 21:00:18 +0000

    agregue algunas que faltaban..... me falto escribir una

--- trunk/queries/constraints/tanteadorConstraints.sql
+++ trunk/queries/constraints/tanteadorConstraints.sql
@@ -3,23 +3,60 @@
 
 DELIMITER $$
 
-DROP TRIGGER IF EXISTS check_maximo_cuatro_cuartos $$
+DROP TRIGGER IF EXISTS check_tanteador_bi $$
 
-CREATE TRIGGER check_maximo_cuatro_cuartos
+CREATE TRIGGER check_tanteador_bi
 BEFORE INSERT ON tanteador
 FOR EACH ROW BEGIN
-  -- Un árbitro en un partido no puede ser de un pais que sea el mismo país que alguno de los equipos.
   DECLARE totalCuartos INT;
   DECLARE cuartoRepetido INT;
+  -- Los valores posibles de TANTEADOR.nroCuatro son = {1 ,2 , 3, 4}
+  CALL sp_valor_en_rango (NEW.nroCuarto, 1, 4, 'tanteador', 'nroCuarto');
+  -- TANTEADOR.scoreEquip1 >= 0
+  -- TANTEADOR.scoreEquip1 >= 0
+  CALL sp_valor_positivo (NEW.scoreEquip1, 'tanteador', 'scoreEquip1');
+  -- TANTEADOR.scoreEquip2 >= 0
+  CALL sp_valor_positivo (NEW.scoreEquip2, 'tanteador', 'scoreEquip2');
+  -- Si TANTEADOR.nroCuarto = 4 => TANTEADOR.scoreEquip1 <> TANTEADOR.scoreEquip2 (no hay empates)
+  
+  -- TANTEADOR.nroCuarto no puede aparecer más de 4 veces por TANTEADOR.idPartido (el tanteador se genera con los 4 cuartos cuando se genera un partido)
   SET totalCuartos = (SELECT count(1) FROM tanteador WHERE idPartido = NEW.idPartido);
-  IF (totalCuartos >= 4)  THEN
+  IF (totalCuartos > 4)  THEN
     CALL `el total de cuartos no puede ser superior a 4`;
   END IF;
   
-  SET cuartoRepetido = (SELECT count(1) FROM tanteador WHERE idPartido = NEW.idPartido and nroCuarto = NEW.nroCuarto);
+  SET cuartoRepetido = (SELECT count(1) FROM tanteador WHERE idPartido = NEW.idPartido AND nroCuarto = NEW.nroCuarto);
   IF (cuartoRepetido = 1)  THEN
     CALL `el cuarto correspondiente ya fue ingresado`;
+  END IF; 
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS check_tanteador_bu $$
+
+CREATE TRIGGER check_tanteador_bu
+BEFORE UPDATE ON tanteador
+FOR EACH ROW BEGIN
+  DECLARE totalCuartos INT;
+  DECLARE cuartoRepetido INT;
+  -- Los valores posibles de TANTEADOR.nroCuatro son = {1 ,2 , 3, 4}
+  CALL sp_valor_en_rango (NEW.nroCuarto, 1, 4, 'tanteador', 'nroCuarto');
+  -- TANTEADOR.scoreEquip1 >= 0
+  CALL sp_valor_positivo (NEW.scoreEquip1, 'tanteador', 'scoreEquip1');
+  -- TANTEADOR.scoreEquip2 >= 0
+  CALL sp_valor_positivo (NEW.scoreEquip2, 'tanteador', 'scoreEquip2');
+  -- Si TANTEADOR.nroCuarto = 4 => TANTEADOR.scoreEquip1 <> TANTEADOR.scoreEquip2 (no hay empates)
+  
+  -- TANTEADOR.nroCuarto no puede aparecer más de 4 veces por TANTEADOR.idPartido (el tanteador se genera con los 4 cuartos cuando se genera un partido)
+  SET totalCuartos = (SELECT count(1) FROM tanteador WHERE idPartido = NEW.idPartido);
+  IF (totalCuartos > 4)  THEN
+    CALL `el total de cuartos no puede ser superior a 4`;
   END IF;
   
+  SET cuartoRepetido = (SELECT count(1) FROM tanteador WHERE idPartido = NEW.idPartido AND nroCuarto = NEW.nroCuarto);
+  IF (cuartoRepetido = 1)  THEN
+    CALL `el cuarto correspondiente ya fue ingresado`;
+  END IF; 
 END$$
+
 DELIMITER ;
