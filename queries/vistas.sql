11d9534b12723c9d81842b00ea7caee1afc897ec
martincammi@gmail.com <martincammi@gmail.com@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-13 21:47:20 +0000

    agregado de puntajes

--- queries/vistas.sql
+++ queries/vistas.sql
@@ -7,23 +7,25 @@ CREATE PROCEDURE partidosDeFase(fase INT)
 BEGIN
 	
 	IF (fase > 0) THEN
-		SELECT partido.idPartido, p1.nombrePais as pais1, p2.nombrePais as pais2, etapa.nombreEtapa as etapa 
-		FROM partido, seleccion s1, seleccion s2, pais p1, pais p2, etapa 
+		SELECT partido.idPartido, p1.nombrePais as pais1, p2.nombrePais as pais2, etapa.nombreEtapa as etapa, tanteador.scoreEquip1 as puntaje1, tanteador.scoreEquip2 as puntaje2 
+		FROM partido, seleccion s1, seleccion s2, pais p1, pais p2, etapa, tanteador 
 		WHERE s1.idSeleccion = equipoSeleccion1 AND
 		 	  s2.idSeleccion = equipoSeleccion2 AND
 		 	  s1.representaPais = p1.idPais AND
 		 	  s2.representaPais = p2.idPais AND
 		 	  partido.juegaEnEtapa = etapa.idEtapa AND
-			  partido.juegaEnEtapa = fase
+			  partido.juegaEnEtapa = fase AND
+			  tanteador.idPartido = partido.IdPartido
 			  ORDER BY idPartido;
 	ELSE
-		SELECT partido.idPartido, p1.nombrePais as pais1, p2.nombrePais as pais2, etapa.nombreEtapa as etapa 
+		SELECT partido.idPartido, p1.nombrePais as pais1, p2.nombrePais as pais2, etapa.nombreEtapa as etapa, tanteador.scoreEquip1 as puntaje1, tanteador.scoreEquip2 as puntaje2 
 		FROM partido, seleccion s1, seleccion s2, pais p1, pais p2, etapa 
 		WHERE s1.idSeleccion = equipoSeleccion1 AND
 		 	  s2.idSeleccion = equipoSeleccion2 AND
 		 	  s1.representaPais = p1.idPais AND
 		 	  s2.representaPais = p2.idPais AND
 		 	  partido.juegaEnEtapa = etapa.idEtapa
+		 	  tanteador.idPartido = partido.IdPartido
 		 	  ORDER BY idPartido;
 	END IF;
 	
