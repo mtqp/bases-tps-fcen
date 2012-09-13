c4c0ef08caf4309012900c79a4c32a0ac44056c7
gemm83@gmail.com <gemm83@gmail.com@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-13 23:41:57 +0000

    sp de arbitros posibles

--- trunk/queries/sps/business_sps.sql
+++ trunk/queries/sps/business_sps.sql
@@ -82,159 +82,158 @@ ON idJugador = idIntegrante
 ORDER BY cantidadPartidos desc;
 END&&
 
--- delimiter &&
+DROP PROCEDURE IF EXISTS sp_posibles_arbitros_por_partidos &&
 
--- DROP PROCEDURE IF EXISTS sp_posibles_arbitros_por_partidos &&
-
--- CREATE PROCEDURE sp_posibles_arbitros_por_partidos(idPartido INT)
--- BEGIN
--- DECLARE  equipo1   INT;
--- DECLARE  equipo2   INT;
--- DECLARE  fecha     DATE;
+CREATE PROCEDURE sp_posibles_arbitros_por_partidos(idPartido INT)
+BEGIN
+DECLARE  equipo1   INT;
+DECLARE  equipo2   INT;
+DECLARE  fecha     DATE;
  
--- SET equipo1 = (SELECT 
---   equipoSeleccion1
--- FROM partido WHERE idPartido = @idPartido);
+SET equipo1 = (SELECT 
+  equipoSeleccion1
+  FROM partido WHERE idPartido = @idPartido);
 
--- SET equipo2 = (SELECT 
---   equipoSeleccion2
---   FROM partido WHERE idPartido = @idPartido);
+SET equipo2 = (SELECT 
+  equipoSeleccion2
+  FROM partido WHERE idPartido = @idPartido);
 
--- SET fecha = (SELECT 
--- fecha
---   FROM partido WHERE idPartido = @idPartido);
+SET fecha = (SELECT 
+  fecha
+  FROM partido WHERE idPartido = @idPartido);
 
--- SELECT arbitro.idArbitro, arbitro.nombreArbitro 
--- FROM arbitro
--- LEFT JOIN
--- (
---   SELECT 
---     SUM(CASE WHEN scOReEquip1 > scOReEquip2 THEN 1 ELSE 0 END) AS ganados,
---     COUNT(1) AS total,
---     equipoSelecciON1 AS equipo,
---     arbitra.idArbitroArb AS arbitro
---   FROM tanteador
--- 	JOIN partido 
---   ON tanteador.idPartido = partido.idPartido
---   AND nroCuarto = 4
---   JOIN arbitra
---   ON arbitra.idPartidoArb = partido.idPartido
---   WHERE equipoSelecciON1 = @equipo1
---   GROUP BY equipoSelecciON1, arbitra.idArbitroArb
---   UNION
---   SELECT 
---     SUM(CASE WHEN scoreEquip1 < scoreEquip2 THEN 1 ELSE 0 END) AS ganados,
---     COUNT(1) AS total,
---     equipoSeleccion2 AS equipo,
---     arbitra.idArbitroArb AS arbitro
---   FROM tanteador
--- 	JOIN partido 
---   ON tanteador.idPartido = partido.idPartido
---   AND nroCuarto = 4
---   JOIN arbitra
---   ON arbitra.idPartidoArb = partido.idPartido
---   WHERE equipoSeleccion2 = @equipo1
---   GROUP BY equipoSeleccion2, arbitra.idArbitroArb
--- ) AS resultadosEquipo1
--- ON 
--- resultadosEquipo1.arbitro = arbitro.idArbitro
--- LEFT JOIN 
--- (
---   SELECT 
---     SUM(CASE WHEN scOReEquip1 > scOReEquip2 THEN 1 ELSE 0 END) AS ganados,
---     COUNT(1) AS total,
---     equipoSelecciON1 AS equipo,
---     arbitra.idArbitroArb AS arbitro
---   FROM tanteador
--- 	JOIN partido 
---   ON tanteador.idPartido = partido.idPartido
---   AND nroCuarto = 4
---   JOIN arbitra
---   ON arbitra.idPartidoArb = partido.idPartido
---   WHERE equipoSelecciON1 = @equipo2
---   GROUP BY equipoSelecciON1, arbitra.idArbitroArb
---   uniON
---   SELECT 
---     SUM(CASE WHEN scOReEquip1 < scOReEquip2 THEN 1 ELSE 0 END) AS ganados,
---     COUNT(1) AS total,
---     equipoSeleccion2 AS equipo,
---     arbitra.idArbitroArb AS arbitro
---   FROM tanteador
--- 	JOIN partido 
---   ON tanteador.idPartido = partido.idPartido
---   AND nroCuarto = 4
---   JOIN arbitra
---   ON arbitra.idPartidoArb = partido.idPartido
---   WHERE equipoSeleccion2 = @equipo2
---   GROUP BY equipoSeleccion2, arbitra.idArbitroArb
--- ) AS resultadosEquipo2
--- ON resultadosEquipo2.arbitro = arbitro.idArbitro
--- WHERE 
-  -- chequeo la primer condicion
---   (
--- 	(
--- 		arbitro.idArbitro =
--- 		CASE WHEN resultadosEquipo1.arbitro IS NOT NULL THEN
--- 		 CASE WHEN resultadosEquipo1.total >= 2 THEN
--- 			CASE WHEN (resultadosEquipo1.ganados < resultadosEquipo1.total) AND (resultadosEquipo1.ganados > 0) then
--- 				arbitro.idArbitro
--- 			ELSE
--- 				-1
--- 			END
--- 		  ELSE
--- 			arbitro.idArbitro
--- 		  END
--- 		ELSE
--- 		  arbitro.idArbitro
--- 		END
---     )
---     AND
---     (
--- 		arbitro.idArbitro =
--- 		CASE WHEN resultadosEquipo2.arbitro IS NOT NULL THEN
--- 		 CASE WHEN resultadosEquipo2.total >= 2 THEN
--- 			CASE WHEN (resultadosEquipo2.ganados < resultadosEquipo2.total) AND (resultadosEquipo2.ganados > 0) then
--- 				arbitro.idArbitro
--- 			ELSE
--- 				-1
--- 			END
--- 		  ELSE
--- 			arbitro.idArbitro
--- 		  END
--- 		ELSE
--- 		  arbitro.idArbitro
--- 		END
---     )
---   )
-  -- chequeo la segunda condicion
---   AND
---   (
---     arbitro.idArbitro = (
--- 		CASE WHEN resultadosEquipo2.arbitro IS NOT NULL AND resultadosEquipo1.arbitro IS NOT NULL THEN
--- 		CASE WHEN resultadosEquipo2.total = 1 AND resultadosEquipo1.total = 1 THEN
--- 	        CASE WHEN resultadosEquipo2.ganados <> resultadosEquipo1.ganados THEN
--- 				arbitro.idArbitro			
--- 			ELSE
--- 				-1
--- 			END
---       ELSE
---         arbitro.idArbitro
---       END
---     ELSE
---       arbitro.idArbitro
---     END
---     )
---   )
-  -- chequeo la condicion 3
---   AND NOT EXISTS (
---     SELECT * FROM partido 
---     JOIN arbitra
---     ON arbitra.idPartidoArb = partido.idPartido
---     WHERE arbitra.idArbitroArb = arbitro.idArbitro
---     AND partido.fecha = @fecha
---   )
--- GROUP BY arbitro.idArbitro, arbitro.nombreArbitro;
--- END&&
+SELECT arbitro.idArbitro, arbitro.nombreArbitro 
+FROM arbitro
+LEFT JOIN
+(
+    SELECT 
+        SUM(CASE WHEN scOReEquip1 > scOReEquip2 THEN 1 ELSE 0 END) AS ganados,
+        COUNT(1) AS total,
+        equipoSelecciON1 AS equipo,
+        arbitra.idArbitroArb AS arbitro
+    FROM tanteador
+    JOIN partido 
+    ON tanteador.idPartido = partido.idPartido
+    AND nroCuarto = 4
+    JOIN arbitra
+    ON arbitra.idPartidoArb = partido.idPartido
+    WHERE equipoSelecciON1 = equipo1
+    GROUP BY equipoSelecciON1, arbitra.idArbitroArb
+    UNION
+    SELECT 
+        SUM(CASE WHEN scoreEquip1 < scoreEquip2 THEN 1 ELSE 0 END) AS ganados,
+        COUNT(1) AS total,
+        equipoSeleccion2 AS equipo,
+        arbitra.idArbitroArb AS arbitro 
+    FROM tanteador
+    JOIN partido 
+    ON tanteador.idPartido = partido.idPartido
+    AND nroCuarto = 4
+    JOIN arbitra
+    ON arbitra.idPartidoArb = partido.idPartido
+    WHERE equipoSeleccion2 = equipo1
+    GROUP BY equipoSeleccion2, arbitra.idArbitroArb
+) AS resultadosEquipo1
+ON 
+resultadosEquipo1.arbitro = arbitro.idArbitro
+LEFT JOIN 
+(
+    SELECT 
+        SUM(CASE WHEN scOReEquip1 > scOReEquip2 THEN 1 ELSE 0 END) AS ganados,
+        COUNT(1) AS total,
+        equipoSelecciON1 AS equipo,
+        arbitra.idArbitroArb AS arbitro
+    FROM tanteador
+    JOIN partido 
+    ON tanteador.idPartido = partido.idPartido
+    AND nroCuarto = 4
+    JOIN arbitra
+    ON arbitra.idPartidoArb = partido.idPartido
+    WHERE equipoSelecciON1 = equipo2
+    GROUP BY equipoSelecciON1, arbitra.idArbitroArb
+    UNION
+    SELECT 
+        SUM(CASE WHEN scOReEquip1 < scOReEquip2 THEN 1 ELSE 0 END) AS ganados,
+        COUNT(1) AS total,
+        equipoSeleccion2 AS equipo,
+        arbitra.idArbitroArb AS arbitro
+    FROM tanteador
+    JOIN partido 
+    ON tanteador.idPartido = partido.idPartido
+    AND nroCuarto = 4
+    JOIN arbitra
+    ON arbitra.idPartidoArb = partido.idPartido
+    WHERE equipoSeleccion2 = equipo2
+    GROUP BY equipoSeleccion2, arbitra.idArbitroArb
+) AS resultadosEquipo2
+ON resultadosEquipo2.arbitro = arbitro.idArbitro
+WHERE 
+-- chequeo la primer condicion (que si a ambos los dirigio mas de dos partidos, que no hayan ganado ni perdidos todos, 
+-- es decir que al menos hayan ganado 1 y que no hayan ganado todos los jugados
+(
+  (
+  arbitro.idArbitro =
+      CASE WHEN resultadosEquipo1.arbitro IS NOT NULL THEN
+        CASE WHEN resultadosEquipo1.total >= 2 THEN
+          CASE WHEN (resultadosEquipo1.ganados < resultadosEquipo1.total) AND (resultadosEquipo1.ganados > 0) then
+            arbitro.idArbitro
+          ELSE
+            -1
+          END
+        ELSE
+          arbitro.idArbitro
+        END
+      ELSE
+        arbitro.idArbitro
+      END
+  )
+  AND
+  (
+  arbitro.idArbitro =
+      CASE WHEN resultadosEquipo2.arbitro IS NOT NULL THEN
+        CASE WHEN resultadosEquipo2.total >= 2 THEN
+          CASE WHEN (resultadosEquipo2.ganados < resultadosEquipo2.total) AND (resultadosEquipo2.ganados > 0) then
+            arbitro.idArbitro
+          ELSE
+            -1
+          END
+        ELSE
+          arbitro.idArbitro
+        END
+      ELSE
+        arbitro.idArbitro
+      END
+  )
+)
+-- chequeo la segunda condicion (si ambos jugaron 1 solo partido con dicho arbitro tienen que tener resultados iguales)
+AND
+(
+  arbitro.idArbitro = (
+    CASE WHEN resultadosEquipo2.arbitro IS NOT NULL AND resultadosEquipo1.arbitro IS NOT NULL THEN
+      CASE WHEN resultadosEquipo2.total = 1 AND resultadosEquipo1.total = 1 THEN
+        CASE WHEN resultadosEquipo2.ganados = resultadosEquipo1.ganados THEN
+          arbitro.idArbitro			
+        ELSE
+          -1
+        END
+      ELSE
+        arbitro.idArbitro
+      END
+    ELSE
+      arbitro.idArbitro
+    END
+  )
+)
+-- chequeo la condicion 3 (si el arbitro esta asignado en la misma fecha)
+AND NOT EXISTS (
+  SELECT * FROM partido 
+  JOIN arbitra
+  ON arbitra.idPartidoArb = partido.idPartido
+  WHERE arbitra.idArbitroArb = arbitro.idArbitro
+  AND partido.fecha = fecha
+)
+GROUP BY arbitro.idArbitro, arbitro.nombreArbitro;
+END&&
 
 delimiter ;
 
