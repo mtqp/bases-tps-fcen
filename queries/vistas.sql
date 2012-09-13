aad458f6d56ca8b40911d8fb3dbee2ad9c134016
martincammi@gmail.com <martincammi@gmail.com@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-09-13 22:38:09 +0000

    sasa

--- queries/vistas.sql
+++ queries/vistas.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE partidosDeFase(fase INT)
 BEGIN
 	
 	IF (fase > 0) THEN
-		SELECT partido.idPartido, p1.nombrePais as pais1, p2.nombrePais as pais2, etapa.nombreEtapa as etapa, tanteador.scoreEquip1 as puntaje1, tanteador.scoreEquip2 as puntaje2 
+		SELECT partido.idPartido, p1.nombrePais as pais1, p2.nombrePais as pais2, etapa.nombreEtapa as etapa, tanteador.scoreEquip1, tanteador.scoreEquip2   
 		FROM partido, seleccion s1, seleccion s2, pais p1, pais p2, etapa, tanteador 
 		WHERE s1.idSeleccion = equipoSeleccion1 AND
 		 	  s2.idSeleccion = equipoSeleccion2 AND
@@ -15,17 +15,56 @@ BEGIN
 		 	  s2.representaPais = p2.idPais AND
 		 	  partido.juegaEnEtapa = etapa.idEtapa AND
 			  partido.juegaEnEtapa = fase AND
-			  tanteador.idPartido = partido.IdPartido
+			  tanteador.idPartido = partido.idPartido AND
+			  tanteador.nroCuarto = 4
 			  ORDER BY idPartido;
 	ELSE
-		SELECT partido.idPartido, p1.nombrePais as pais1, p2.nombrePais as pais2, etapa.nombreEtapa as etapa, tanteador.scoreEquip1 as puntaje1, tanteador.scoreEquip2 as puntaje2 
-		FROM partido, seleccion s1, seleccion s2, pais p1, pais p2, etapa 
+		SELECT partido.idPartido, p1.nombrePais as pais1, p2.nombrePais as pais2, etapa.nombreEtapa as etapa, tanteador.scoreEquip1, tanteador.scoreEquip2 
+		FROM partido, seleccion s1, seleccion s2, pais p1, pais p2, etapa, tanteador 
 		WHERE s1.idSeleccion = equipoSeleccion1 AND
 		 	  s2.idSeleccion = equipoSeleccion2 AND
 		 	  s1.representaPais = p1.idPais AND
 		 	  s2.representaPais = p2.idPais AND
-		 	  partido.juegaEnEtapa = etapa.idEtapa
-		 	  tanteador.idPartido = partido.IdPartido
+		 	  partido.juegaEnEtapa = etapa.idEtapa AND
+		 	  tanteador.idPartido = partido.idPartido AND
+			  tanteador.nroCuarto = 4
+		 	  ORDER BY idPartido;
+	END IF;
+	
+
+END&&
+
+DROP PROCEDURE IF EXISTS partidosDeFaseArbitro &&
+
+CREATE PROCEDURE partidosDeFaseArbitro (fase INT)
+BEGIN
+	
+	IF (fase > 0) THEN
+		SELECT arbitro.nombreArbitro, partido.idPartido, p1.nombrePais as pais1, p2.nombrePais as pais2, 
+			   etapa.nombreEtapa as etapa, 
+			   tanteador.scoreEquip1, tanteador.scoreEquip2   
+		FROM partido, seleccion s1, seleccion s2, pais p1, pais p2, etapa, tanteador, arbitra, arbitro 
+		WHERE s1.idSeleccion = equipoSeleccion1 AND
+		 	  s2.idSeleccion = equipoSeleccion2 AND
+		 	  s1.representaPais = p1.idPais AND
+		 	  s2.representaPais = p2.idPais AND
+		 	  partido.juegaEnEtapa = etapa.idEtapa AND
+			  partido.juegaEnEtapa = fase AND
+			  tanteador.idPartido = partido.idPartido AND
+			  tanteador.nroCuarto = 4 AND
+			  arbitra.idArbitroArb = arbitro.idArbitro AND
+			  arbitra.idPartidoArb = partido.idPartido
+			  ORDER BY idPartido;
+	ELSE
+		SELECT partido.idPartido, p1.nombrePais as pais1, p2.nombrePais as pais2, etapa.nombreEtapa as etapa, tanteador.scoreEquip1, tanteador.scoreEquip2 
+		FROM partido, seleccion s1, seleccion s2, pais p1, pais p2, etapa, tanteador 
+		WHERE s1.idSeleccion = equipoSeleccion1 AND
+		 	  s2.idSeleccion = equipoSeleccion2 AND
+		 	  s1.representaPais = p1.idPais AND
+		 	  s2.representaPais = p2.idPais AND
+		 	  partido.juegaEnEtapa = etapa.idEtapa AND
+		 	  tanteador.idPartido = partido.idPartido AND
+			  tanteador.nroCuarto = 4
 		 	  ORDER BY idPartido;
 	END IF;
 	
