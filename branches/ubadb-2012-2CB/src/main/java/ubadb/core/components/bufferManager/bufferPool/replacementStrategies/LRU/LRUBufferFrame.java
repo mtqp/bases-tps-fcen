2530768a32e05066e116d2bdb7e1daff4b5c8aa8
martincammi <martincammi@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-10-24 23:52:07 +0000

    LRU and MRU

---
+++ branches/ubadb-2012-2CB/src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/LRU/LRUBufferFrame.java
@@ -0,0 +1,32 @@
+package ubadb.core.components.bufferManager.bufferPool.replacementStrategies.LRU;
+
+import java.util.Date;
+
+import ubadb.core.common.Page;
+import ubadb.core.components.bufferManager.bufferPool.BufferFrame;
+import ubadb.core.exceptions.BufferFrameException;
+
+public class LRUBufferFrame extends BufferFrame {
+	private Date referenceDate;
+
+	public LRUBufferFrame(Page page) {
+		super(page);
+		referenceDate = new Date();
+	}
+
+	public Date getReferenceDate() {
+		return referenceDate;
+	}
+
+	// llamo al correcto y actualizo la fecha
+	public void pin() {
+		super.pin();
+		referenceDate = new Date();
+	}
+
+	// llamo al correcto y actualizo la fecha
+	public void unpin() throws BufferFrameException {
+		super.unpin();
+		referenceDate = new Date();
+	}
+}
---
+++ branches/ubadb-2012-2CB/src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/LRU/LRUReplacementStrategy.java
@@ -0,0 +1,38 @@
+package ubadb.core.components.bufferManager.bufferPool.replacementStrategies.LRU;
+
+import java.util.Collection;
+import java.util.Date;
+
+import ubadb.core.common.Page;
+import ubadb.core.components.bufferManager.bufferPool.BufferFrame;
+import ubadb.core.components.bufferManager.bufferPool.replacementStrategies.PageReplacementStrategy;
+import ubadb.core.exceptions.PageReplacementStrategyException;
+
+public class LRUReplacementStrategy implements PageReplacementStrategy {
+	public BufferFrame findVictim(Collection<BufferFrame> bufferFrames)
+			throws PageReplacementStrategyException {
+		LRUBufferFrame victim = null;
+		Date oldestReplaceablePageDate = null;
+
+		for (BufferFrame bufferFrame : bufferFrames) {
+			LRUBufferFrame mruBufferFrame = (LRUBufferFrame) bufferFrame;
+			if (mruBufferFrame.canBeReplaced()
+					&& (oldestReplaceablePageDate == null || mruBufferFrame
+							.getReferenceDate().before(
+									oldestReplaceablePageDate))) {
+				victim = mruBufferFrame;
+				oldestReplaceablePageDate = mruBufferFrame.getReferenceDate();
+			}
+		}
+
+		if (victim == null)
+			throw new PageReplacementStrategyException(
+					"No page can be removed from pool");
+		else
+			return victim;
+	}
+
+	public BufferFrame createNewFrame(Page page) {
+		return new LRUBufferFrame(page);
+	}
+}
