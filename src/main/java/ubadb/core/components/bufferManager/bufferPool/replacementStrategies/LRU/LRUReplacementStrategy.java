bb8b8579a9d891d101bcede77c6bba8cd6379326
noeliaserapio@gmail.com <noeliaserapio@gmail.com@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-11-28 02:59:36 +0000

    Volvi a agregar el test que sacaron y el que agregaron ustedes lo puse abajo de todo. Subo de nuevo las versiones de mru y lru que habia subido el sabado pasado.

--- src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/LRU/LRUReplacementStrategy.java
+++ src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/LRU/LRUReplacementStrategy.java
@@ -1,9 +1,7 @@
 package ubadb.core.components.bufferManager.bufferPool.replacementStrategies.LRU;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 
 import ubadb.core.common.Page;
 import ubadb.core.components.bufferManager.bufferPool.BufferFrame;
@@ -12,39 +10,33 @@ import ubadb.core.components.bufferManager.bufferPool.replacementStrategies.Page
 import ubadb.core.exceptions.PageReplacementStrategyException;
 
 public class LRUReplacementStrategy implements PageReplacementStrategy {
-	
 	public BufferFrame findVictim(Collection<BufferFrame> bufferFrames)
 			throws PageReplacementStrategyException {
-		List<ReferenceBufferFrame> leastRecentlyUsedFrames = new ArrayList<ReferenceBufferFrame>();
-		
-		int minPinCount = Integer.MAX_VALUE;
-		for (BufferFrame bufferFrame : bufferFrames) {
-			if(bufferFrame.getPinCount() < minPinCount){
-				minPinCount = bufferFrame.getPinCount();
-			}
-		}
-		
-		for (BufferFrame bufferFrame : bufferFrames) {
-			if(bufferFrame.getPinCount() == minPinCount){
-				leastRecentlyUsedFrames.add((ReferenceBufferFrame)bufferFrame);
-			}
-		}
-		
 		ReferenceBufferFrame victim = null;
-		Date recentReplaceablePageDate = null;
+		Date oldestReplaceablePageDate = null;
 
-		for (ReferenceBufferFrame leastUsedBufferFrame : leastRecentlyUsedFrames) {
+		for (BufferFrame bufferFrame : bufferFrames) {
+			ReferenceBufferFrame lruBufferFrame = (ReferenceBufferFrame) bufferFrame;
 			
-			if (recentReplaceablePageDate == null || leastUsedBufferFrame.getReferenceDate().before(recentReplaceablePageDate)) {
-				victim = leastUsedBufferFrame;
-				recentReplaceablePageDate = leastUsedBufferFrame.getReferenceDate();
+			// Si encuentra una pagina que nunca fue referenciada, la devuelve y sale del for sin seguir buscando
+			if(lruBufferFrame.canBeReplaced() && lruBufferFrame.getReferenceDate() == null ) {
+				victim = lruBufferFrame;
+				break;
+			}
+			
+			if (lruBufferFrame.canBeReplaced()
+					&& (oldestReplaceablePageDate == null || lruBufferFrame
+							.getReferenceDate().before(
+									oldestReplaceablePageDate))) {
+				victim = lruBufferFrame;
+				oldestReplaceablePageDate = lruBufferFrame.getReferenceDate();
 			}
 			
 		}
-		
-		//Siempre deberia encontrar una victima salvo que la lista de bufferFrames sea vacia
+
 		if (victim == null)
-			throw new PageReplacementStrategyException("No page can be removed from pool");
+			throw new PageReplacementStrategyException(
+					"No page can be removed from pool");
 		else
 			return victim;
 	}
--- src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/MRU/MRUReplacementStrategy.java
+++ src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/MRU/MRUReplacementStrategy.java
@@ -1,9 +1,7 @@
 package ubadb.core.components.bufferManager.bufferPool.replacementStrategies.MRU;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 
 import ubadb.core.common.Page;
 import ubadb.core.components.bufferManager.bufferPool.BufferFrame;
@@ -16,40 +14,34 @@ public class MRUReplacementStrategy implements PageReplacementStrategy {
 			throws PageReplacementStrategyException {
 		ReferenceBufferFrame victim = null;
 		Date recentReplaceablePageDate = null;
-		List<ReferenceBufferFrame> mostRecentlyUsedFrames = new ArrayList<ReferenceBufferFrame>();
-		
-		int maxPinCount = 0;
+
 		for (BufferFrame bufferFrame : bufferFrames) {
-			if(bufferFrame.getPinCount() > maxPinCount){
-				maxPinCount = bufferFrame.getPinCount();
-			}
-		}
-		
-		for (BufferFrame bufferFrame : bufferFrames) {
-			if(bufferFrame.getPinCount() == maxPinCount){
-				mostRecentlyUsedFrames.add((ReferenceBufferFrame)bufferFrame);
-			}
-		}
-		
-		for (ReferenceBufferFrame mostUsedBufferFrame : mostRecentlyUsedFrames) {
+			ReferenceBufferFrame mruBufferFrame = (ReferenceBufferFrame) bufferFrame;
 			
-			if (recentReplaceablePageDate == null || mostUsedBufferFrame.getReferenceDate().after(recentReplaceablePageDate)) {
-				victim = mostUsedBufferFrame;
-				recentReplaceablePageDate = mostUsedBufferFrame.getReferenceDate();
+			// Si encuentra una pagina que nunca fue referenciada, la devuelve y sale del for sin seguir buscando
+			if(mruBufferFrame.canBeReplaced() && mruBufferFrame.getReferenceDate() == null ) {
+				victim = mruBufferFrame;
+				break;
+			}
+			
+			if (mruBufferFrame.canBeReplaced()
+					&& (recentReplaceablePageDate == null || mruBufferFrame
+							.getReferenceDate()
+							.after(recentReplaceablePageDate))) {
+				victim = mruBufferFrame;
+				recentReplaceablePageDate = mruBufferFrame.getReferenceDate();
 			}
 			
 		}
-		
-		//Siempre deberia encontrar una victima salvo que la lista de bufferFrames sea vacia
+
 		if (victim == null)
-			throw new PageReplacementStrategyException("No page can be removed from pool");
+			throw new PageReplacementStrategyException(
+					"No page can be removed from pool");
 		else
 			return victim;
 	}
 
 	public BufferFrame createNewFrame(Page page) {
 		return new ReferenceBufferFrame(page);
-		//Seria necesario hacer un pin como que se cargo a la cache? asumimos que no porque quizas
-		//como en el caso de un filescan, la pagina se subio a caché pero no se uso.
 	}
 }
