0dbfcc5e75c77345c94059fbaaa66088cde95e89
martincammi <martincammi@9d84415c-6767-5fe0-eec0-06918237f7b4>
2012-11-29 04:13:02 +0000

    Algunos tests más para el TouchCount

--- src/test/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/TouchCount/TouchCountReplacementStrategyTest.java
+++ src/test/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/TouchCount/TouchCountReplacementStrategyTest.java
@@ -8,20 +8,19 @@ import org.junit.Before;
 import org.junit.Test;
 
 import ubadb.core.components.bufferManager.bufferPool.BufferFrame;
-import ubadb.core.components.bufferManager.bufferPool.replacementStrategies.fifo.FIFOReplacementStrategy;
 import ubadb.core.exceptions.PageReplacementStrategyException;
 import ubadb.core.testDoubles.DummyObjectFactory;
 import ubadb.core.util.TestUtil;
 
-
 public class TouchCountReplacementStrategyTest
 {
-	private FIFOReplacementStrategy strategy;
+	private TouchCountReplacementStrategy strategy;
 	
 	@Before
-	public void setUp()
-	{
-		strategy = new FIFOReplacementStrategy();
+	public void setUp(){
+		int countIntervalSeconds = 1;
+		int agingHotCriteria = 2;
+		strategy = new TouchCountReplacementStrategy(countIntervalSeconds, agingHotCriteria);
 	}
 	
 	@Test(expected=PageReplacementStrategyException.class)
@@ -48,30 +47,98 @@ public class TouchCountReplacementStrategyTest
 		
 		assertEquals(frame2,strategy.findVictim(Arrays.asList(frame0,frame1,frame2)));
 	}
-	
+
+	//Martin: La creacion del frame no genera fecha con lo que los sleep están de más.
 	@Test
 	public void testMultiplePagesToReplace() throws Exception
 	{
 		BufferFrame frame0 = strategy.createNewFrame(DummyObjectFactory.PAGE);
-		Thread.sleep(TestUtil.PAUSE_INTERVAL);	//Add a sleep so that frame dates are different
 		BufferFrame frame1 = strategy.createNewFrame(DummyObjectFactory.PAGE);
-		Thread.sleep(TestUtil.PAUSE_INTERVAL);
 		BufferFrame frame2 = strategy.createNewFrame(DummyObjectFactory.PAGE);
 		
 		assertEquals(frame0,strategy.findVictim(Arrays.asList(frame0,frame1,frame2)));
 	}
 
+	//Martin: La creacion del frame no genera fecha con lo que los sleep están de más.
 	@Test
 	public void testMultiplePagesToReplaceButOldestOnePinned() throws Exception
 	{
 		BufferFrame frame0 = strategy.createNewFrame(DummyObjectFactory.PAGE);
-		Thread.sleep(TestUtil.PAUSE_INTERVAL);	//Add a sleep so that frame dates are different
 		BufferFrame frame1 = strategy.createNewFrame(DummyObjectFactory.PAGE);
-		Thread.sleep(TestUtil.PAUSE_INTERVAL);
 		BufferFrame frame2 = strategy.createNewFrame(DummyObjectFactory.PAGE);
 		
 		frame0.pin();
 		
 		assertEquals(frame1,strategy.findVictim(Arrays.asList(frame0,frame1,frame2)));
 	}
+	
+	/**
+	 * First frame with high aging
+	 * @throws Exception
+	 */
+	@Test
+	public void testFirstWithHighAging() throws Exception{
+		BufferFrame frame0 = strategy.createNewFrame(DummyObjectFactory.PAGE);
+		BufferFrame frame1 = strategy.createNewFrame(DummyObjectFactory.PAGE);
+		
+		touchIt(frame0, 2);
+		
+		assertEquals(frame1,strategy.findVictim(Arrays.asList(frame0,frame1)));
+	}
+	
+	
+	/**
+	 * Firsts frames with high aging
+	 * @throws Exception
+	 */
+	@Test
+	public void testFirstsWithHighAging() throws Exception{
+		BufferFrame frame0 = strategy.createNewFrame(DummyObjectFactory.PAGE);
+		BufferFrame frame1 = strategy.createNewFrame(DummyObjectFactory.PAGE);
+		BufferFrame frame2 = strategy.createNewFrame(DummyObjectFactory.PAGE);
+		BufferFrame frame3 = strategy.createNewFrame(DummyObjectFactory.PAGE);
+		
+		touchIt(frame0, 4);
+		touchIt(frame1, 2);
+		touchIt(frame2, 1);
+		touchIt(frame3, 2);
+		
+		assertEquals(frame2,strategy.findVictim(Arrays.asList(frame0,frame1,frame2,frame3)));
+	}
+	
+	/**
+	 * All with high touchcount
+	 * @throws Exception
+	 */
+	@Test(expected=PageReplacementStrategyException.class)
+	public void testAllWithTouchCountHigh() throws Exception{
+		BufferFrame frame0 = strategy.createNewFrame(DummyObjectFactory.PAGE);
+		BufferFrame frame1 = strategy.createNewFrame(DummyObjectFactory.PAGE);
+		BufferFrame frame2 = strategy.createNewFrame(DummyObjectFactory.PAGE);
+		
+		touchIt(frame0, 2);
+		touchIt(frame1, 2);
+		touchIt(frame2, 2);
+		
+		strategy.findVictim(Arrays.asList(frame0,frame1,frame2));
+	}
+	
+	/**
+	 * Pins and unpin the frame any amount of times. (Will touch it only once)
+	 * @param frame the frame to be pin-unpin
+	 * @param amount the amount of times
+	 * @throws Exception
+	 */
+	private void touchIt(BufferFrame frame, int amount) throws Exception{
+		
+		int i = amount;
+		while(i > 0){
+			frame.pin();
+			frame.unpin();
+			Thread.sleep(TestUtil.SLEEP_ONE_SECOND);
+			i--;
+		}
+	}
+	
+	
 }
